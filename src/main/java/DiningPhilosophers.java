24da25ee0a24d3c991e8b4775eac63f53581e8fa
nguyenviettduy <nguyenviettduy@gmail.com>
2020-04-12 22:15:24 +0700

    first commit

---
+++ src/main/java/DiningPhilosophers.java
@@ -0,0 +1,31 @@
+public class DiningPhilosophers {
+
+    public static void main(String[] args) throws Exception {
+
+        Philosopher[] philosophers = new Philosopher[5];
+        Object[] forks = new Object[philosophers.length];
+
+        for (int i = 0; i < forks.length; i++) {
+            forks[i] = new Object();
+        }
+
+        for (int i = 0; i < philosophers.length; i++) {
+            Object leftFork = forks[i];
+            Object rightFork = forks[(i + 1) % forks.length];
+
+            /**
+             * Nhà triết học có số hiệu lẻ lấy đũa bên trái, rồi bên phải.
+             * Ngược lại đối với các nhà triết học có số hiệu chẵn
+             */
+
+            if(i % 2 == 0){
+                philosophers[i] = new Philosopher(rightFork, leftFork);
+            }
+            else {
+                philosophers[i] = new Philosopher(leftFork, rightFork);
+            }
+            Thread t = new Thread(philosophers[i], "Philosopher " + (i + 1));
+            t.start();
+        }
+    }
+}
---
+++ src/main/java/Philosopher.java
@@ -0,0 +1,33 @@
+public class Philosopher implements Runnable {
+    private Object leftFork;
+    private Object rightFork;
+
+    public Philosopher(Object leftFork, Object rightFork) {
+        this.leftFork = leftFork;
+        this.rightFork = rightFork;
+    }
+
+    private void doAction(String action){
+        System.out.println(Thread.currentThread().getName() + " " + action);
+        try {
+            Thread.sleep((int)Math.random() * 100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            doAction(System.nanoTime() + ": Thinking");
+            synchronized (leftFork) {
+                doAction(System.nanoTime() + ": Picked up left fork");
+                synchronized (rightFork) {
+                    doAction(System.nanoTime() + ": Picked up right fork - eating");
+                    doAction(System.nanoTime() + ": Put down right fork");
+                }
+                doAction(System.nanoTime() + ": Put down left fork. Back to thinking");
+            }
+        }
+    }
+}
